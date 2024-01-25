90e712e471ea4ebc95104ddd684a81dda1ec6b32
Suha Son <dhkd98@gmail.com>
2024-01-25 14:37:54 +0900

    daily commit

---
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/test/java/com/suha/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suha.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
