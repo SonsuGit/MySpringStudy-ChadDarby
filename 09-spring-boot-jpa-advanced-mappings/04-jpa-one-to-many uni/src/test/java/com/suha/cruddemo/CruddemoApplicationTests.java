00af19775e1517ba29581eb7fad3fabc0611b365
Suha Son <dhkd98@gmail.com>
2024-02-28 01:02:04 +0900

    daily commit

---
+++ 09-spring-boot-jpa-advanced-mappings/04-jpa-one-to-many uni/src/test/java/com/suha/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suha.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
